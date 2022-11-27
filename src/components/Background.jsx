7eebc0a3c3d8d7fd94d6fcf4ea04d0e5697d80bb
Giao Vu Dinh <tvudinh20@gmail.com>
2022-11-27 14:23:42 -0500

    background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -62,6 +62,66 @@ export const Background = () => {
           </p>
         </div>
       </div>
+      <div className="flex-container">
+        <div className="flex-item">
+          <p style={{ margin: 0 }}>
+            Fisher was diagnosed with bipolar disorder at the age of 24. Around
+            the time of <i>Return of the Jedi’s</i> release in 1983, Fisher
+            began to notice that she had become increasingly dependent on drug
+            use. After months of sobriety, she accidentally overdosed on a
+            combination of prescription medication and sleeping pills in 1985.
+            According to Fisher, following her diagnosis, “drugs made [her] feel
+            more normal.” She stated that “having had this illness [her] entire
+            life, [she] accommodated it by developing a very big personality.”
+            Fisher often remarked that she wrote about her diagnosis a lot to
+            make light of it as a way of survival (Bose, 2020).
+            <br />
+            <br />
+            According to Fisher’s interview with <i>bp Magazine</i>, following
+            her diagnosis she remained casual about talk therapy and
+            noncommittal about medication, but she abandoned treatment in the
+            late 1990s, following her breakup with talent agent Brian Lourd
+            (Stephens, 2020b). This led her to experience an intense array of
+            symptoms, including “the heady energy of mania, the self-absorption,
+            the sleepless nights, the talking jags when she’d keep people on the
+            phone for hours — and, inevitably, the soul-shriveling drops into
+            depression.” (Stephens, 2020b). Fisher was ultimately hospitalized
+            following a psychotic break in 1997.
+            <br />
+            <br />
+            Fisher has been very open about her experiences with bipolar
+            disorder. In her interview with <i>bp Magazine</i>, she shared that
+            she occasionally powered through her mood episodes, such as enduring
+            a depressive episode while working on <i>Wishful Drinking.</i> In
+            other instances, her mood episodes have been involuntarily displayed
+            to the public: a 2013 video was widely publicized showing Fisher
+            “losing focus and rambling while performing on a Caribbean cruise
+            ship” (Stephens, 2020a). Fisher later stated that she had been in a
+            very severe manic state, bordering on psychosis. According to her,
+            she “screwed up on [her] medications, and stopped sleeping, and
+            couldn’t stop talking or writing” (Stephens, 2020a).
+            <br />
+            <br />
+            Fisher has also been vocal about the importance of a strong support
+            system in her life, and how her close family members and friends
+            have adjusted to and been able to support her. In particular,
+            Fisher’s daughter, Billie Lourd, grew up “on permanent alert” for
+            signs that her mother was slipping into a manic episode: “reckless
+            behavior, overeating, overspending, irritability, and bouts of
+            anger” (Stephens, 2020a). On the other hand, Fisher’s recurring
+            bouts of medication, which she described as “medication-proof and
+            bullet-proof,” led her to be a fan of regular ECT (Stephens, 2020a).
+            <br />
+            <br />
+          </p>
+        </div>
+        <div className="flex-item__image">
+          <img
+            src="https://hips.hearstapps.com/esq.h-cdn.co/assets/16/52/1482861791-carrie-fisher.jpg?resize=480:*"
+            alt="carrie fisher"
+          />
+        </div>
+      </div>
     </div>
   );
 };
